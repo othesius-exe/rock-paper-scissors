7975f76516bb44913b88b4ccbdb3e7650d17ab00
Caleb Imler <calebqimler@gmail.com>
2020-06-12 13:48:51 -0400

    I changed some things

--- rps.js
+++ rps.js
@@ -1,7 +1,6 @@
-let computerSelection;
-let playerSelection;
 /* Determine the computer's move */
 function computerPlay() {
+    let computerSelection;
     let randomNumber = Math.random().toFixed(2);    
     if (randomNumber <= 0.33) {
         computerSelection = 'Rock';
@@ -10,56 +9,72 @@ function computerPlay() {
     } else {
         computerSelection = 'Scissors';
     }
-    return computerSelection
+    return computerSelection.toLowerCase();
 }
 
 /* Get Player's selection */
 function getPlayerSelection() {
-    playerSelection = prompt(`Rock, Paper, or Scissors?`, '');
+    let playerSelection = prompt(`Rock, Paper, or Scissors?`, '').toLowerCase();
     return playerSelection
 }
 
 /* Play one round */
  function quickPlay() {
-    getPlayerSelection();
-    computerPlay();
-    playerSelection.toLowerCase();
-    computerSelection.toLowerCase();
-    console.log(typeof(computerSelection));
-    console.log(typeof(playerSelection));
+    const playerSelection = getPlayerSelection();
+    const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
 }
 
 /* Play 5 rounds */
 function game() {
+    let i = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+    let numberOfDraws = 0;
+    while (i < 5) {
+        let result = playRound(getPlayerSelection(), computerPlay())
+        if (result.includes('You Win')) {
+            playerScore++;
+            return result;
+        } else if (result.includes('You Lose')) {
+            computerScore++;
+            return result;
+        } else {
+            numberOfDraws++;
+            return result;
+        }
+    } i++;
 
+    if (playerScore > computerScore) {
+        console.log(`You Win! Player: ${playerScore} to Computer: ${computerScore}: draws: ${numberOfDraws}`);
+    } else if (playerScore === computerScore) {
+        console.log(`It's a draw. Player: ${playerScore} to Computer: ${computerScore}: draws: ${numberOfDraws}`);
+    } else {
+        console.log(`You lose! Player: ${playerScore} to Computer: ${computerScore}: draws: ${numberOfDraws} `);
+    }
 }
+
 
 /* Compare Players selection to Computer's selection */
 function playRound(playerSelection, computerSelection) {
+    let result = '';
     if (playerSelection != null && playerSelection != undefined && playerSelection != '') {
         if (playerSelection == computerSelection) {
-            return "It\'s a draw!";
+            result = "It\'s a draw!";
         } else if ((playerSelection == 'rock') && (computerSelection == 'paper')){
-            computerScore++;
-            return "You Lose. Paper beats rock.";
+            result = "You Lose. Paper beats rock.";
         } else if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
-            playerScore++;
-            return "You win! Rock beats scissors";
+            result = "You Win! Rock beats scissors";
         }  else if ((playerSelection == 'paper') && (computerSelection == 'rock')){
-            playerScore++;
-            return "You win! Paper beats rock.";
+            result ="You Win! Paper beats rock.";
         } else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
-            computerScore++;
-            return "You lose. Scissors beats paper";
+            result = "You Lose. Scissors beats paper";
         }  else if ((playerSelection == 'scissors') && (computerSelection == 'paper')){
-            playerScore++;
-            return "You Lose. Paper beats rock.";
+            result = "You Lose. Paper beats rock.";
         } else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
-            computerScore++;
-            return "You lose. Rock beats scissors.";
+            result = "You Lose. Rock beats scissors.";
         } else {
-            return 'Something went wrong';
+            result = 'Something went wrong';
         }
-    }
+    } return result; 
 }
